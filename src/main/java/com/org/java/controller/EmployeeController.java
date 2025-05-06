4f28103cf1faa44deca772483fa3d77d8996a8fa
techigit <srinugithub88@gmail.com>
2025-05-06 13:55:44 +0530

    5Y4WY

--- src/main/java/com/org/java/controller/EmployeeController.java
+++ src/main/java/com/org/java/controller/EmployeeController.java
@@ -37,7 +37,7 @@ public class EmployeeController {
 	public String welcomeTest() {
 		logger.trace("it is trace method welcome calling");
 		logger.info("wel come method is working");
-		return "WELCOME TO NEW JOURNEY OF SPRING BOOT TOMCAT PIPELINE INTEGRATION TEST";
+		return "WELCOME TO SPRINGBOOT TOMCAT INTEGRATION TEST 2.0";
 	}
 
 	@PostMapping("/save")
